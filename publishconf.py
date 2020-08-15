08e54777f2d43243152ba9aa2e3519c3268fbb92
Sam Roeca <samuel.roeca@gmail.com>
2020-08-15 13:53:49 -0400

    Add samroeca.com to url pointing

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://softwarejourneyman.com'
+SITEURL = 'https://samroeca.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
