cee2d03b29e104c2a113e7e2d5f18dc68739cbf5
Sam Roeca <samuel.roeca@gmail.com>
2020-09-10 14:31:54 -0700

    Move plugins to modern system

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,44 +2,26 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+# helpers
+#######################################################################
+_CUSTOM_CSS = "static/custom.css"
+
+# core
+#######################################################################
 AUTHOR = "Samuel Roeca"
 SITENAME = "Sam's Blog"
-
-JINJA_ENVIRONMENT = {
-    "extensions": [
-        "jinja2.ext.i18n",
-    ],
-}
-
 PATH = "content"
-
 TIMEZONE = "EST"
-
 DEFAULT_LANG = "en"
 I18N_TEMPLATES_LANG = "en"
-
-# Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
 DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
-
-# How to extract data from filename
-# Currently, only does the slug
-# FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
 FILENAME_METADATA = "(?P<slug>.*)"
-
-# Ignore caching
 LOAD_CONTENT_CACHE = False
-
-# static pathes for publishing
-_CUSTOM_CSS = "static/custom.css"
 EXTRA_PATH_METADATA = {
     "extra/custom.css": {
         "path": _CUSTOM_CSS,
@@ -52,29 +34,17 @@ STATIC_PATHS = [
     "extra",
 ]
 
-# Code blocks
-PYGMENTS_RST_OPTIONS = {
-    # 'linenos': 'inline',  # enable line numbers
-}
-
+# theme
 #######################################################################
-# Flex theme-specific configurations
-#######################################################################
-
 THEME = "pelican-themes/Flex"
-
 FAVICON = "/theme/images/favicon.ico"
 PYGMENTS_STYLE = "tango"
-
 SITESUBTITLE = "Sam's blog"
 SITEDESCRIPTION = "Samuel Roeca's blog"
 SITEURL = "http://localhost:8000"
 SITELOGO = "/images/sam-headshot-kepler-300x300.jpg"
-
 MAIN_MENU = True
-
 CUSTOM_CSS = _CUSTOM_CSS
-
 SOCIAL = (
     ("github", "https://github.com/pappasam"),
     ("linkedin-square", "https://www.linkedin.com/in/samuel-roeca-23010735"),
@@ -82,11 +52,10 @@ SOCIAL = (
     ("twitter", "https://twitter.com/SamRoeca"),
     (
         "youtube",
-        "https://www.youtube.com/channel/UCjORdFKiDlqzzW7bWYqXuKA?view_as=subscriber",
+        "https://www.youtube.com/channel/UCjORdFKiDlqzzW7bWYqXuKA",
     ),
     ("envelope-o", "mailto:samuel.roeca@gmail.com"),
 )
-
 DIRECT_TEMPLATES = [
     "index",
     "categories",
@@ -94,13 +63,11 @@ DIRECT_TEMPLATES = [
     "archives",
     "tags",
 ]
-
 MENUITEMS = (
     ("Archives", "/archives.html"),
     ("Categories", "/categories.html"),
     ("Tags", "/tags.html"),
 )
-
 MARKDOWN = {
     "extension_configs": {
         "markdown.extensions.codehilite": {
@@ -114,26 +81,17 @@ MARKDOWN = {
             "marker": "[TOC]",
             "toc_depth": 3,
         },
-        "mdx_include": {
-            "base_path": "./content/articles"
-        },
+        "mdx_include": {"base_path": "./content/articles"},
     },
     "output_format": "html5",
 }
 
+# plugins
 #######################################################################
-# Pelican Plugins
-#######################################################################
-
-PLUGIN_PATHS = ["pelican-plugins"]
 PLUGINS = [
-    "i18n_subsites",
     "sitemap",
     "neighbors",
-    "assets",  # minifies css assets
 ]
-
-# sitemap
 SITEMAP = {
     "format": "xml",
     "priorities": {
--- publishconf.py
+++ publishconf.py
@@ -9,11 +9,8 @@ from pelicanconf import *
 
 SITEURL = 'https://samroeca.com'
 RELATIVE_URLS = False
-
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
-
 DELETE_OUTPUT_DIRECTORY = False
-
 DISQUS_SITENAME = "pappasam-github-io"
 GOOGLE_ANALYTICS = "UA-117115805-1"
